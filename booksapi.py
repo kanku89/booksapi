5b5d5d48a4f6e3a10107e4a6869826c549e12cfc
konrad.fijalkowski <fijalkowski.konrad@gmail.com>
2018-03-28 15:12:09 +0200

    Poprawki wyglądu, rozpoczęcie struktury bazodanowej.

--- booksapi.py
+++ booksapi.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-
+import sqlite3
 
 
 class Search():
@@ -10,6 +10,8 @@ class Search():
 
         self.frame_search = ttk.Frame(master)
         master.title('Books API')
+
+#Wygląd searcha
 
         self.frame_search.pack()
 
@@ -22,15 +24,47 @@ class Search():
         self.author = ttk.Entry(self.frame_search)
         self.title = ttk.Entry(self.frame_search)
         self.genre = ttk.Combobox(self.frame_search, values = ('To', 'Jest', 'Kurde', 'Dramat'))
+        self.rating = ttk.Combobox(self.frame_search, values = ('1', '2', '3', '4', '5'))
 
         self.author.grid(row = 1, column = 0)
         self.title.grid(row= 1, column=1)
+
         self.genre.grid(row= 1, column=3)
+        self.rating.grid(row=1, column=4)
 
+        self.search = ttk.Button(self.frame_search, text='Szukaj')
         self.add_new = ttk.Button(self.frame_search, text='Dodaj nową')
-        self.add_new.pack()
+        self.add_new.grid(row=3, column =0, sticky = 'nw')
+        self.search.grid(row=2, column = 0, sticky = 'nw')
 
-        self.frame_show
+ #   def pokaz_dane(self):
+  #      (self.author.get())
+
+
+
+#funkcja czyszczenia
+
+    def clear(self):
+        self.author.delete(0, 'end')
+        self.title.delete(0, 'end')
+
+    def db_search(self):
+        db = sqlite3.connect()
+
+        cur = db.cursor()
+        cur.execute()
+
+
+#Wygląd wyników wyszukiwania
+
+        #self.frame_show = ttk.Frame(master)
+
+
+  #  def conditions(self):
+
+
+
+
 
 def main():
 
@@ -39,4 +73,4 @@ def main():
     root.mainloop()
 
 
-if __name__ == "__main__": main()
+if __name__ == '__main__': main()
