6d3b41c62c24f6723138410b4eb16162cf308a98
konrad.fijalkowski <fijalkowski.konrad@gmail.com>
2018-03-29 16:42:36 +0200

    Aktualne na czas wyjścia z pracy.

--- booksapi.py
+++ booksapi.py
@@ -17,18 +17,22 @@ class Search():
         ttk.Label(self.frame_search, text= 'Tytuł:').grid(row = 0, column = 1, sticky = 'sw')
         ttk.Label(self.frame_search, text= 'Zakres dat:').grid(row = 0, column = 2, sticky = 'sw')
         ttk.Label(self.frame_search, text= 'Gatunek:').grid(row = 0, column = 3, sticky = 'sw')
-        ttk.Label(self.frame_search, text= 'Ocena:').grid(row = 0, column = 4, sticky = 'sw')
+        ttk.Label(self.frame_search, text= 'Liczba stron:').grid(row = 0, column = 4, sticky = 'sw')
+
+        ttk.Label(self.frame_search, text= 'Ocena:').grid(row = 0, column = 5, sticky = 'sw')
 
         self.author = ttk.Entry(self.frame_search)
         self.title = ttk.Entry(self.frame_search)
+        self.pages = ttk.Entry(self.frame_search)
         self.genre = ttk.Combobox(self.frame_search, values = ('To', 'Jest', 'Kurde', 'Dramat'))
         self.rating = ttk.Combobox(self.frame_search, values = ('1', '2', '3', '4', '5'))
 
         self.author.grid(row = 1, column = 0)
         self.title.grid(row= 1, column=1)
+        self.pages.grid(row = 1, column = 4)
 
         self.genre.grid(row= 1, column=3)
-        self.rating.grid(row=1, column=4)
+        self.rating.grid(row=1, column=5)
 
         self.search = ttk.Button(self.frame_search, text='Szukaj')
         self.add_new = ttk.Button(self.frame_search, text='Dodaj nową', command = self.pokaz_dane)
@@ -38,8 +42,19 @@ class Search():
     def pokaz_dane(self):
         print(self.author.get())
 
-    INSERT INTO 'BOOKS'('AUTHOR', 'TITLE', 'DATE_START', 'DATE_END', 'PAGES', 'REVIEW', 'SUMMARY', 'SPENT', 'RATING')
-    VALUES('self.author.get()', 'self.title.get()', 'self.date_start.get()',
+
+'''
+DODAWANIE NOWEJ KSIĄŻKI
+
+"INSERT INTO 'BOOKS'('AUTHOR', 'TITLE', 'DATE_START', 'DATE_END', 'PAGES', 'REVIEW', 'SUMMARY', 'SPENT', 'RATING') "
+"VALUES('self.author.get()', 'self.title.get()', 'self.date_start.get()', 'self.date_end.get()', 'self.pages.get()',
+" 'self.review.get()','self.summary.get()', 'SPĘDZONO CZASU' 'self.rating.get()' "
+
+
+'''
+
+
+    #SELECT * FROM BOOKS;
 
     #funkcja czyszczenia
 
@@ -50,7 +65,7 @@ class Search():
     def db_insert(self):
         db_con = functions.connection()
         cur = db_con.cursor()
-        sql = "SELECT * FROM BOOKS"
+        sql = ()
         cur.execute(sql)
 
 
