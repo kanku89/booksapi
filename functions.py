100ce76716ae0e283b9a9d67d178c67373726a15
Konrad <fijalkowski.konrad@gmail.com>
2018-04-26 10:07:03 +0200

    Nadal nie działa wyświetlanie nowego okna, ale pojawiło się trochę zmian.

--- functions.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-def connection():
-    con = pyodbc.connect("DRIVER={MySQL ODBC 5.3 Unicode Driver};SERVER=de2.fcomet.com;PORT=3306;"
-                         "DATABASE=yugenpin_booksapi;UID=yugenpin_bookapp;PASSWORD=Booksapi@123;")
-    return con
-
-def close_window(Frame):
-    Frame.destroy()
